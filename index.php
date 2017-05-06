220fcb35db950e704a2b5e6cc2d8557b57e6a74c
REMIR Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2017-05-06 23:18:03 +0200

    Ask The Question ?

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>UI-X # Osmar # X-IU</title>
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<form method="get" class="saerch-box-form" action="https://google.com/search">
+			<input class="search-box-input" type="text" name="q" placeholder="Ask the Question ?" autofocus="autofocus"/>
+		</form>
+
+
+	<footer>
+		(c) NaDa - Usurpator Of The Osmaria Throne, 5X. No Right Reserved
+	</footer>
+	</body>
+</html>
